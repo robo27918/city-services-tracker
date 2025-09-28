566985cc802b8385a6a375fea2b43b7071ffe4ab
robo27918 <roberttoribio04@gmail.com>
2025-09-28 00:00:36 -0700

    adds fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_28_062038) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_28_065110) do
   create_table "issues", force: :cascade do |t|
     t.string "issue_type"
     t.string "location"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_28_062038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_issues_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_28_062038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "issues", "users"
 end
