57d5412e1561491c47024e23b34b7d498808eb74
robo27918 <roberttoribio04@gmail.com>
2025-09-28 20:43:50 -0700

    added views and controllers and added bootstrap

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,50 @@
+class IssuesController < ApplicationController
+  before_action :set_issue , only: %i[show edit update destroy]
+  def index
+    @issues = Issue.all # an array of ActiveRecord:: Relation, each element is an issue object
+  end
+
+  def show
+  end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def edit
+  end
+
+  def create
+    @issue = Issue.new(issue_params)
+    if @issue.save
+      redirect_to @issue, notice:"Issue created successfully!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @issue.update(issue_params)
+      redirect_to @issue, notice: "Issue updated successfully!"
+    else
+      render:edit
+    end
+  end
+  
+  def destroy
+    @issue.destroy
+    redirect_to issues_path,notice: "Issue deleted successfully!"
+  end
+  
+  private
+  
+  def set_issue
+    @issue= Issue.find(params[:id])
+  end
+  
+  def issue_params
+    params.require(:issue).permit(:description, :status, :issue_type, :user_id)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :issues
+  get "issues/index"
+  get "issues/show"
+  get "issues/new"
+  get "issues/edit"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
